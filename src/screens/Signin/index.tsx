0b6a759b82aecbe81d749c011e762f97e0653728
cintiaelisa@gmail.com <cintiaelisa@gmail.com>
2021-06-24 23:44:25 -0300

    Aula 02 - Maximum Speed

--- src/screens/Signin/index.tsx
+++ src/screens/Signin/index.tsx
@@ -4,15 +4,18 @@ import { Text, View, Image, StatusBar } from 'react-native';
 import { ButtonIcon } from '../../components/Buttonicon';
 import IllustrationImg from '../../assets/illustration.png';
 import { styles } from './styles';
+import { useNavigation } from '@react-navigation/native';
 
 export function SignIn() {
+
+    const navigation = useNavigation();
+
+    function handleSigIn() {
+        navigation.navigate('Home');
+    }
+
   return (
       <View style={styles.container}>
-        <StatusBar 
-            barStyle="light-content"
-            backgroundColor="transparent"
-            translucent
-        />
         <Image 
             source={IllustrationImg}
             style={styles.image}
@@ -21,19 +24,20 @@ export function SignIn() {
 
         <View style={styles.content}>
             <Text style={styles.title}>
-                Organize {`\n`}
-                suas joganinas {`\n`}
-                facilmente
+                Conecte-se {'\n'}
+                e organize suas  {'\n'}
+                jogatinas
             </Text>
 
             <Text style={styles.subtitle}>
-                Crie grupos para jogar seus games {`\n`}
+                Crie grupos para jogar seus games {'\n'}
                 favoritos com seus amigos
             </Text>
 
             <ButtonIcon 
                 title="Entrar com Discord"
-                activeOpacity={0.7}/>
+                onPress={handleSigIn}
+            />
         
         </View>
       </View>    
